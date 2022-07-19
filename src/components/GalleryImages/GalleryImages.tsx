540f54a09492acd3b5b989ff80632acda5983a67
Victor Braz <victor.90braz@gmail.com>
2022-07-19 17:11:24 +0200

    useInterface added

--- src/components/GalleryImages/GalleryImages.tsx
+++ src/components/GalleryImages/GalleryImages.tsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import GalleryImagesStyle from "./GalleryImagesStyle";
 
+const Fruits {
+  name: "Smoothie bowl";
+  url: "https://media.istockphoto.com/photos/smoothie-bowl-with-frozen-berriesand-flowers-on-white-background-very-picture-id1364075968?s=612x612";
+  description: "Smoothie bowl with frozen berries,and flowers on white background. Very Peri color of the year 2022. top view. healthy breakfast";
+  price: 8.50;
+}
+
+interface IFruits {
+  name: string;
+  url: string;
+  description: string;
+  price: number;
+}
+
 const GalleryImages = () => {
+  const {fruit, setFruits} = useState<IFruits>(Fruits)
   return (
     <GalleryImagesStyle>
       <h3>Search for photos in our image bank for commercial use</h3>
